9452a3edcd16567f7756fc3695d92d787ef3a4cc
alx <37true@gmail.com>
2019-01-06 07:27:32 +0700

    fragments

--- app/src/main/java/tk/alltrue/testfragment/MainActivity.java
+++ app/src/main/java/tk/alltrue/testfragment/MainActivity.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView imCat;
+    private TextView tCat;
+    private int mText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,19 +17,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imCat = (ImageView) findViewById(R.id.imageView);
-
+        tCat = findViewById(R.id.textView);
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button1:
                 imCat.setImageResource(R.drawable.ryzh);
+                tCat.setText("Ryzhik");
                 break;
             case R.id.button2:
                 imCat.setImageResource(R.drawable.bars);
+                tCat.setText("Barsik");
                 break;
             case R.id.button3:
                 imCat.setImageResource(R.drawable.murz);
+                tCat.setText("Murzik");
                 break;
         }
     }
