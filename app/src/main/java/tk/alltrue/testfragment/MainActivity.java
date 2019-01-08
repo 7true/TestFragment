46204f20960d9cb18f2aa7048a72abc07ae9f7b5
alx <37true@gmail.com>
2019-01-08 14:29:19 +0700

    fragment chapter 2

--- app/src/main/java/tk/alltrue/testfragment/MainActivity.java
+++ app/src/main/java/tk/alltrue/testfragment/MainActivity.java
@@ -1,12 +1,13 @@
 package tk.alltrue.testfragment;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Fragment1.OnSelectedButtonListener{
     private ImageView imCat;
     private TextView tCat;
     private int mText;
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+/*
         imCat = (ImageView) findViewById(R.id.imageView);
         tCat = findViewById(R.id.textView);
     }
@@ -34,6 +35,18 @@ public class MainActivity extends AppCompatActivity {
                 imCat.setImageResource(R.drawable.murz);
                 tCat.setText("Murzik");
                 break;
+        }*/
+    }
+
+    @Override
+    public void onButtonSelected(int buttonIndex) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        Fragment2 fragment2 = (Fragment2) fragmentManager.findFragmentById(R.id.fragment2);
+
+        if(fragment2 != null) {
+            fragment2.setDescription(buttonIndex);
         }
     }
+
 }
